075795d44f83f1accafb491408adcfe721c60a2c
Rigid <rigid93@gmail.com>
2021-09-30 18:49:08 +0200

    Initial commit

---
+++ src/main/java/com/rigid/email/RigidEMailApplication.java
@@ -0,0 +1,13 @@
+package com.rigid.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigidEMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RigidEMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rigid/email/RigidEMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rigid.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RigidEMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
